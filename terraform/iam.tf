b675b7006dde23a6103fc9dc8866c0e3054515cd
Akash <skyme5997@gmail.com>
2025-07-30 18:58:52 +0530

    iam file

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,3 +1,4 @@
+# CodeDeploy Role
 resource "aws_iam_role" "codedeploy_role" {
   name = "CodeDeployServiceRole"
   assume_role_policy = jsonencode({
@@ -17,6 +18,7 @@ resource "aws_iam_role_policy_attachment" "codedeploy_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
 }
 
+# CodeBuild Role
 resource "aws_iam_role" "codebuild_role" {
   name = "CodeBuildServiceRole"
   assume_role_policy = jsonencode({
@@ -36,6 +38,7 @@ resource "aws_iam_role_policy_attachment" "codebuild_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
 }
 
+# EC2 Role
 resource "aws_iam_role" "ec2_role" {
   name = "EC2InstanceRole"
   assume_role_policy = jsonencode({
@@ -58,4 +61,24 @@ resource "aws_iam_instance_profile" "ec2_instance_profile" {
 resource "aws_iam_role_policy_attachment" "ec2_ssm_policy" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# ✅ CodePipeline Role (this was missing)
+resource "aws_iam_role" "codepipeline_role" {
+  name = "CodePipelineServiceRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "codepipeline.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codepipeline_policy" {
+  role       = aws_iam_role.codepipeline_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodePipelineFullAccess"
 }
