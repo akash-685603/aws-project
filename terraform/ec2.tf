7672108cad78cae81e93b98ca9c294563aed2b4e
Akash <skyme5997@gmail.com>
2025-07-30 18:47:55 +0530

    final changes

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,10 +1,21 @@
+resource "aws_instance" "web" {
+  ami                    = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI (for us-east-1)
+  instance_type          = "t2.micro"
+  key_name               = var.key_name
+  subnet_id              = module.vpc.public_subnets[0]
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2_instance_profile.name
+
+  tags = {
+    Name = "WebServer"
+  }
+}
 resource "aws_security_group" "web_sg" {
   name        = "web-sg"
   description = "Allow HTTP and SSH"
   vpc_id      = module.vpc.vpc_id
 
   ingress {
-    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -12,7 +23,6 @@ resource "aws_security_group" "web_sg" {
   }
 
   ingress {
-    description = "HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -24,9 +34,5 @@ resource "aws_security_group" "web_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "web-sg"
   }
 }
--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,19 +1,22 @@
-resource "aws_iam_role" "codebuild_role" {
-  name = "codebuild-role"
+resource "aws_iam_role" "codedeploy_role" {
+  name = "codedeploy-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "codebuild.amazonaws.com"
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Principal = {
+          Service = "codedeploy.amazonaws.com"
+        },
+        Effect = "Allow",
+        Sid    = ""
       }
-      Action = "sts:AssumeRole"
-    }]
+    ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "codebuild_attach" {
-  role       = aws_iam_role.codebuild_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
+resource "aws_iam_role_policy_attachment" "codedeploy_attach" {
+  role       = aws_iam_role.codedeploy_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
 }
