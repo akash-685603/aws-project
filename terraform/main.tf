fb1ee655ae186403b460768b84928565c03c4eef
Akash <skyme5997@gmail.com>
2025-07-30 18:51:41 +0530

    updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,3 @@
 provider "aws" {
   region = "us-east-1"
 }
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.2"
-
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-east-1a"]
-  public_subnets  = ["10.0.1.0/24"]
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "my-vpc"
-  }
-}
